c0a2de7256018be705e77a698d7e285dfd207bf0
Jihye Kwon <ggung0929@gmail.com>
2023-04-11 00:41:06 +0900

    4.10 self

--- myprofile/js/main.js
+++ myprofile/js/main.js
@@ -1,11 +1,11 @@
-// window.onload = function (){
-//     // 디지털 시계
-//     setInterval(setWatch,1000);
-//     function setWatch(){
-//         const date = new Date();
-//         let now = date.toLocaleTimeString();
-//         document.getElementById("demo").innerHTML = now;
-//     }
+window.onload = function (){
+    // 디지털 시계
+    setInterval(setWatch,1000);
+    function setWatch(){
+        const date = new Date();
+        let now = date.toLocaleTimeString();
+        document.getElementById("demo").innerHTML = now;
+    }
 //     // 배경 이미지 change
 //     let picture = ["./images/header1.jpg", "./images/header2.jpg", "./images/header3.jpg"];
 //     let imgIdx=0;
@@ -19,4 +19,4 @@
 //         }
 //         setTimeout(showPicture,2000);
 //     }
-// }
+}
